8d064b1a40ed18dd9b9abd5ace5d3d44acbdc0a2
epinotes <epinotes@gmail.com>
2019-08-29 15:53:08 +0000

    add OD definitions to runshny help

--- R/runshiny.R
+++ R/runshiny.R
@@ -1,6 +1,70 @@
 #' @title Run the Shiny application
 #' @description Launch a browser for uploading the file
 #'   with ICD-10-CM.
+#' @details 
+#' 
+#' Below are the ICD10-CM Definitions of the Overdoses
+
+#' 1. Any drug:
+#'   Any diagnosis of T36-T50
+#' 
+#' AND a 6th character of 1,2,3, or 4 for T36.9, T37.9, T39.9, T41.4, T42.7, T43.9, T45.9, T47.9, and T49.9, or a 5th character of 1,2,3, or 4 for all the others T36-T50
+#' 
+#' AND a 7th character of A or missing
+#' 
+#' 2. Any Opioid:
+#'   Any diagnosis of T40.0X, T40.1X, T40.2X, T40.3X, T40.4X, T40.60, T40.60
+#' 
+#' AND a 6th character of 1,2,3, or 4 
+#' 
+#' AND a 7th character of A or missing
+#' 
+#' 
+#' 3. Heroin:
+#'   Any diagnosis of T40.1X
+#' 
+#' AND a 6th character of 1,2,3, or 4 
+#' 
+#' AND a 7th character of A or missing
+#' 
+#' 
+#' 4. Non-Heroin Opioid:
+#'   Any diagnosis of T40.0X, T40.2X, T40.3X, T40.4X,T40.60, T40.69
+#' 
+#' AND a 6th character of 1,2,3, or 4 
+#' 
+#' AND a 7th character of A or missing
+#' 
+#' Then exclude any cases of heroin as defined above
+#' 
+#' 
+#' 5. Stimulant:
+#'   Any diagnosis of T40.5X, T43.60, T43.61, T43.62, T43.63, T43.64, T43.69
+#' 
+#' AND a 6th character of 1,2,3, or 4 
+#' 
+#' AND a 7th character of A or missing
+#' 
+#' 
+#' 6. Cocaine:
+#'   Any diagnosis of T40.5X
+#' 
+#' AND a 6th character of 1,2,3, or 4 
+#' 
+#' AND a 7th character of A or missing
+#' 
+#' 
+#' 7. Non-Cocaine Stimulant:
+#'   Any diagnosis of T43.60, T43.61, T43.62, T43.63, T43.64, T43.69
+#' 
+#' AND a 6th character of 1,2,3, or 4 
+#' 
+#' AND a 7th character of A or missing
+#' 
+#' Then exclude any cases of cocaine as defined above
+#' 
+#' 
+
 #' @export runshiny
 #' @examples
 #' \dontrun{
